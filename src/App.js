29403fd5c9d7764ab90bdd56e646b4760447c131
Gnanaraj C <allwin.gnanaraj-c@capgemini.com>
2024-07-25 18:18:37 +0530

    App.js and PageNotFound update

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 
 import './App.css';
-import {BrowserRouter as Router, Route, Routes, Switch} from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes, Switch } from 'react-router-dom';
 import Login from '../src/pages/Login';
 import Navbar from '../src/pages/Navbar';
 import Register from '../src/pages/Register';
@@ -11,25 +11,27 @@ import FlightCard from './pages/FlightCard';
 import ResetPassword from './pages/ResetPassword';
 import Admin from './pages/Admin';
 import { FlightProvider } from './pages/Context/FlightContextProvide';
+import PageNotFound from './pages/PageNotFound';
 
 function App() {
   return (
     <div className=""  >
       <FlightProvider>
-       <Navbar/>
-       <Router>
-        <Routes>
-          <Route path='/' element={<Login/>}/>
-          <Route path='/register' element={<Register/>}/>
-          <Route path='/forgotPassword' element={<Forgot/>}/>
-          <Route path="/searchPage" element={<SearchPage/>} />
-        <Route path="/results" element={<ResultsPage />} />
-        <Route path="/flight/:id" element={<FlightCard />} />
-        <Route path="resetPassword/:email" element={<ResetPassword />} />
-        <Route path="/Admin" element={<Admin />} />
-        </Routes>
-       </Router>
-       </FlightProvider>
+        <Navbar />
+        <Router>
+          <Routes>
+            <Route path='/' element={<Login />} />
+            <Route path='/register' element={<Register />} />
+            <Route path='/forgotPassword' element={<Forgot />} />
+            <Route path="/searchPage" element={<SearchPage />} />
+            <Route path="/results" element={<ResultsPage />} />
+            <Route path="/flight/:id" element={<FlightCard />} />
+            <Route path="resetPassword/:email" element={<ResetPassword />} />
+            <Route path="/Admin" element={<Admin />} />
+            <Route path='*' element={<PageNotFound />} />
+          </Routes>
+        </Router>
+      </FlightProvider>
     </div>
   );
 }
